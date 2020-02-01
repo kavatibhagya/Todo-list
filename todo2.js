459844da110f016dbdb3b690f28ca2556374cda2
kavatibhagya <bhagyalakshmi.kavati@gmail.com>
2020-01-31 20:42:07 -0800

    added basic todo list normal version

--- todo2.js
+++ todo2.js
@@ -1,9 +1,11 @@
-const newTask = document.querySelector('.new-task');
-const addlist = document.querySelector('.ul-list');
-  
+
 
 function addTolist(){
+    const newTask = document.querySelector('#new-task');
+    const addlist = document.querySelector('#ul-list');
+  
     const list =document.createElement('li');
-    list.innerText = newTask.Value;
+    list.innerText = newTask.value;
     addlist.appendChild(list);
+    newTask.value ='';
 }
